83f1e4a901dbdfa7f0bab2d5c640d83410e25c7c
marcospls21 <marcossouza_19@hotmail.com>
2026-02-05 17:13:12 -0300

    ajuste providers

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,28 +1,15 @@
 terraform {
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-    helm = {
-      source  = "hashicorp/helm"
-      version = "~> 2.9"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.20"
-    }
+    aws        = { source = "hashicorp/aws", version = "~> 5.0" }
+    helm       = { source = "hashicorp/helm", version = "~> 2.9" }
+    kubernetes = { source = "hashicorp/kubernetes", version = "~> 2.20" }
   }
-  backend "local" {
-    path = "terraform.tfstate"
-  }
+  backend "local" { path = "terraform.tfstate" }
 }
 
-provider "aws" {
-  region = "us-east-1"
-}
+provider "aws" { region = "us-east-1" }
 
-# Configuração para conectar no EKS nativo
+# Providers configurados com o endpoint nativo
 provider "kubernetes" {
   host                   = aws_eks_cluster.this.endpoint
   cluster_ca_certificate = base64decode(aws_eks_cluster.this.certificate_authority[0].data)
